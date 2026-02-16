cbc603ab4babe7b5594420081894b4ae08ea4064
saravanan1702 <saravananarumugam1702@gmail.com>
2026-02-16 17:02:25 +0530

    updated to ios visual features

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalIterm}>
+    <View style={styles.goalIterm}>
+      <Pressable
+        android_ripple={{ color: "#4800a7" }}
+        onPress={props.onDeleteItem.bind(this, props.id)}
+        style={({ pressed }) => pressed && styles.pressedItem}
+      >
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -17,11 +21,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalIterm: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
